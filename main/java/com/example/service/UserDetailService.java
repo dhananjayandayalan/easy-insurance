91488df01f7f7b6adeb45027041ce2ce61af66ed
prashant nath <prashant.nath@avizva.com>
2021-11-22 16:13:51 +0530

    sever-service-class-prashant

---
+++ main/java/com/example/service/UserDetailService.java
@@ -0,0 +1,14 @@
+package com.example.server.service;
+
+
+
+
+import com.example.server.entities.UserDetail;
+
+public interface UserDetailService {
+
+
+    public UserDetail.UserDetail addUserDetail(UserDetail userDetail);
+
+}
+
---
+++ main/java/com/example/service/UserDetailServiceImpl.java
@@ -0,0 +1,11 @@
+package com.example.server.service;
+
+import com.example.server.entities.UserDetail;
+
+public class UserDetailServiceImpl implements UserDetailService {
+
+    @Override
+    public UserDetail.UserDetail addUserDetail(UserDetail userDetail) {
+        return null;
+    }
+}
