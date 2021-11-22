7396f91c4a69bd2675969d792ba75008af89b39c
Dhananjayan D <dhananjayan.dayalan@gmail.com>
2021-11-22 16:20:14 +0530

    Fresh Initial Commit

---
+++ MyOrdersCard.js
@@ -0,0 +1,14 @@
+import classes from './my_orders_card.module.css'
+
+
+const MyOrdersCard=()=>{
+    return(
+
+        <div className={classes.my_orders_card}>
+            
+        </div>
+
+    )
+}
+
+export default MyOrdersCard;
